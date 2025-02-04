2b45e861f0b0d940c0148d0e2bd63973fe4d1928
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-04 16:39:59 +0900

    create: model

---
+++ JJalng/BuyHistory.swift
@@ -0,0 +1,26 @@
+//
+//  BuyHistory.swift
+//  JJalng
+//
+//  Created by Saebyeok Jang on 2/4/25.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+final class BuyHistory: Identifiable {
+    var id = UUID()
+    var memo: String = ""
+    var category: Category?
+    var date: Date?
+    var createAt: Date?
+    
+    init(id: UUID = UUID(), memo: String, category: Category? = nil, date: Date? = nil, createAt: Date? = nil) {
+        self.id = id
+        self.memo = memo
+        self.category = category
+        self.date = date
+        self.createAt = createAt
+    }
+}
---
+++ JJalng/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  JJalng
+//
+//  Created by Saebyeok Jang on 2/4/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
